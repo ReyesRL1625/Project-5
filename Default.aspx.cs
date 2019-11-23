66e89a07035421f67765dd6b2147381329bfa265
unknown <ReyesRL1625@gmail.com>
2019-11-23 11:48:52 -0700

    "Initial commit"

---
+++ Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project_5_Web_App
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void btnCamp_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("http://neptune.fulton.ad.asu.edu/roboticscamp/");
+        }
+        protected void btnStaff_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Protected/Staff.aspx"); // staff page
+        }
+        protected void btnStaffLogin_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Login.aspx"); // Staff login page
+        }
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Account/StudentRegister.aspx");
+        }
+        protected void btnStudentLogin_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Account/StudentLogin.aspx");
+        }
+    }
+
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project_5_Web_App.Global" Language="C#" %>
