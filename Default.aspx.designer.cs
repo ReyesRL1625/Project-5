2632fbdb0b5cd1d7e3ef65480ce7b84f74760fbd
Zadig Alvarez <zalvarez@sundevils.asu.edu>
2019-11-24 13:32:17 -0700

    Default page redirects to staff and member page with buttons

--- Default.aspx.designer.cs
+++ Default.aspx.designer.cs
@@ -11,5 +11,23 @@ namespace Project_5_Web_App {
     
     
     public partial class Default {
+        
+        /// <summary>
+        /// MemberButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button MemberButton;
+        
+        /// <summary>
+        /// StaffButton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button StaffButton;
     }
 }
