13107bee80315280c74fcadbe4dec24089f84335
cdlopez9 <42393035+cdlopez9@users.noreply.github.com>
2019-11-24 15:44:24 -0700

    Checking for merge conflicts

--- Protected/Staff.aspx
+++ Protected/Staff.aspx
@@ -1,15 +1,24 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Staff.aspx.cs" Inherits="Project_5_Web_App.Protected.Staff" %>
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Staff.aspx.cs" Inherits="Project_5_Web_App.Protected.Staff" %>
 
-<!DOCTYPE html>
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title></title>
-</head>
-<body>
-    <form id="form1" runat="server">
+<asp:Content ID="Content3" ContentPlaceHolderID="MainContent" Runat="Server">
+    <h1>Staff Page</h1> <br />    
+    
+    <li>
+        <p >Enter a username or password to add or remove a member</p>
+         <div>
+             <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>  
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>  
+            <asp:Button ID="Button1" runat="server" OnClick="btn_addMember" Text="Add" />
+            <asp:Button ID="Button2" runat="server" OnClick="btn_rmMember" Text="Remove" />
+         </div>
         <div>
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>  
+            <asp:TextBox ID="TextBox4" runat="server"></asp:TextBox>  
+            <asp:Button ID="Button3" runat="server" OnClick="btn_addStaff" Text="Add" />
+            <asp:Button ID="Button4" runat="server" OnClick="btn_rmStaff" Text="Remove" />
         </div>
-    </form>
-</body>
-</html>
+        </li>
+    
+</asp:Content>
+
--- Protected/Staff.aspx.cs
+++ Protected/Staff.aspx.cs
@@ -4,14 +4,74 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace Project_5_Web_App.Protected
 {
     public partial class Staff : System.Web.UI.Page
     {
+        StaffMembers StaffMembersList = new StaffMembers();
+        Members MemberList = new Members();
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btn_addMember(object sender, EventArgs e)
+        {
+            String text1 = TextBox1.Text;
+            String text2 = TextBox2.Text;
+
+            var member = new List<Member>
+            {
+                    new Member { username = text1, password = text2 }
+            };
+
+            ToXmlFile(member, "Members.xml");
+        }
+        protected void btn_rmMember(object sender, EventArgs e)
+        {
+
+            String text1 = TextBox1.Text;
+            String text2 = TextBox2.Text;
+        }
+        protected void btn_addStaff(object sender, EventArgs e)
+        {
+            String text1 = TextBox3.Text;
+            String text2 = TextBox4.Text;
+        }
+        protected void btn_rmStaff(object sender, EventArgs e)
+        {
+            String text1 = TextBox3.Text;
+            String text2 = TextBox4.Text;
+        }
+
+        public class StaffMembers
+        {
+            public List<Member> Member { get; set; }
+        }
+        public class Members
+        {
+            public List<Member> Member { get; set; }
+        }
+        public class Member
+        {
+            public string username { get; set; }
+            public string password { get; set; }
+        }
+        
+        public void ToXmlFile(Object obj, string filePath)
+        {
+            var xs = new XmlSerializer(obj.GetType());
+            var ns = new XmlSerializerNamespaces();
+            var ws = new XmlWriterSettings { Indent = true, OmitXmlDeclaration = true };
+            ns.Add("", "");
+
+            using (XmlWriter writer = XmlWriter.Create(Server.MapPath(filePath), ws))
+            {
+                xs.Serialize(writer, obj);
+            }
         }
     }
 }
